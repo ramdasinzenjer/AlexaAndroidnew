4f36815093782d57ece273c8c8c5f1e51f2a510c
Will Blaschko <will.blaschko@gmail.com>
2016-02-28 19:23:37 -0800

    add finish to MainActivity

--- app/src/main/java/com/willblaschko/android/alexavoicelibrary/MainActivity.java
+++ app/src/main/java/com/willblaschko/android/alexavoicelibrary/MainActivity.java
@@ -1,5 +1,7 @@
 package com.willblaschko.android.alexavoicelibrary;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     Currently empty because we don't really have anything to put in here,
     this would be where we show our normal launch activity which can be anything
      */
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //nothing to see here, bye bye!
+        this.finish();
+    }
 }
