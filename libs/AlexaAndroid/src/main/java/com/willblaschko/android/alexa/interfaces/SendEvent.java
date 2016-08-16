c490d662104723730c578613d20b6e791f798053
Will Blaschko <will.blaschko@gmail.com>
2016-08-16 09:11:25 -0700

    use cache dir instead

--- libs/AlexaAndroid/src/main/java/com/willblaschko/android/alexa/interfaces/SendEvent.java
+++ libs/AlexaAndroid/src/main/java/com/willblaschko/android/alexa/interfaces/SendEvent.java
@@ -90,6 +90,7 @@ public abstract class SendEvent {
     }
 
     private AvsResponse parseResponse() throws IOException, AvsException, RuntimeException {
+
         Request request = mRequestBuilder.build();
 
         currentCall = mClient.newCall(request);
